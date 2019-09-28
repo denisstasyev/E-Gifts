6221b6b4a4318cdb0c19a5665e81cc4375dc8d43
Denis Stasyev <denisstasyev@gmail.com>
2019-09-28 16:41:21 +0300

    Cancel Eject and add Material UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import { createBrowserHistory } from "history";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "assets/scss/material-kit-react.scss?v=1.8.0";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// pages for this product
+import LandingPage from "views/LandingPage/LandingPage.js";
+
+// var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <BrowserRouter>
+  {/* <Router history={hist}> */}
+    <Switch>
+      <Route path="/" component={LandingPage} />
+    </Switch>
+  {/* </Router> */}
+  </BrowserRouter>,
+  document.getElementById("root")
+);
