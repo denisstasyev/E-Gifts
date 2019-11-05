3f8ad2aadb051c2c7ba9593f2c6aeda1bde9453d
Denis Stasyev <denisstasyev@gmail.com>
2019-11-06 00:55:19 +0300

    Do NotFound page

--- frontend/src/containers/NotFound/NotFound.js
+++ frontend/src/containers/NotFound/NotFound.js
@@ -28,48 +28,54 @@ const NotFound = () => {
       <div className={classes.container}>
         <Container>
           <Header topic="Not Found" />
-          <Box id="content" my={2}>
-            <Box className={classes.box} p={2}>
-              <Typography className={classes.title} variant="h5">
-                What happened?
-              </Typography>
-              <Typography>
-                Sorry, 404 Error occurred: this page does not exist yet. Visit
-                Gallery to see available E-Gifts
-              </Typography>
-              <div className={classes.fab}>
-                <Fab
-                  variant="extended"
-                  size="medium"
-                  color="primary"
-                  component={Link}
-                  to="/home"
-                >
-                  <GalleryIcon className={classes.icon} />
-                  Gallery
-                </Fab>
+          <Box id="content">
+            <div className={classes.boxes}>
+              <div className={classes.box1}>
+                <Box className={classes.boxLogo} p={2} mt={2}>
+                  <Typography className={classes.flexTitle} variant="h5">
+                    What happened?
+                  </Typography>
+                  <Typography className={classes.flexText}>
+                    Sorry, 404 Error occurred: this page does not exist yet.
+                    Visit Gallery to see available E-Gifts
+                  </Typography>
+                  <div className={classes.fab}>
+                    <Fab
+                      variant="extended"
+                      size="medium"
+                      color="primary"
+                      component={Link}
+                      to="/home"
+                    >
+                      <GalleryIcon className={classes.icon} />
+                      Gallery
+                    </Fab>
+                  </div>
+                </Box>
               </div>
-            </Box>
-            <Box className={classes.box} p={2} mt={2}>
-              <Typography className={classes.title} variant="h5">
-                First time here?
-              </Typography>
-              <Typography>
-                Learn more about the E-Gifts project on the Home page
-              </Typography>
-              <div className={classes.fab}>
-                <Fab
-                  variant="extended"
-                  size="medium"
-                  color="primary"
-                  component={Link}
-                  to="/gallery"
-                >
-                  <HomeIcon className={classes.icon} />
-                  Home
-                </Fab>
+              <div className={classes.box2}>
+                <Box className={classes.boxLogo} p={2} mt={2}>
+                  <Typography className={classes.flexTitle} variant="h5">
+                    First time here?
+                  </Typography>
+                  <Typography className={classes.flexText}>
+                    Learn more about the E-Gifts project on the Home page
+                  </Typography>
+                  <div className={classes.fab}>
+                    <Fab
+                      variant="extended"
+                      size="medium"
+                      color="primary"
+                      component={Link}
+                      to="/gallery"
+                    >
+                      <HomeIcon className={classes.icon} />
+                      Home
+                    </Fab>
+                  </div>
+                </Box>
               </div>
-            </Box>
+            </div>
           </Box>
         </Container>
       </div>
--- frontend/src/containers/NotFound/styles.js
+++ frontend/src/containers/NotFound/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-import { BORDER_RADIUS } from "configCSS";
+import { MOBILE_WIDTH, BORDER_RADIUS } from "configCSS";
 
 export const useStyles = makeStyles(theme => ({
   root: {
@@ -15,15 +15,42 @@ export const useStyles = makeStyles(theme => ({
     overflowY: "auto",
     height: "100%"
   },
-  box: {
-    backgroundColor: theme.palette.background.paper,
-    borderRadius: BORDER_RADIUS
+  boxes: {
+    display: "flex",
+    flexWrap: "wrap"
   },
-  title: {
+  box1: {
+    width: "50%",
+    paddingRight: theme.spacing(1),
+    // eslint-disable-next-line
+    [`@media (max-width: ${MOBILE_WIDTH}px)`]: {
+      paddingRight: 0,
+      width: "100%"
+    }
+  },
+  box2: {
+    width: "50%",
+    paddingLeft: theme.spacing(1),
+    // eslint-disable-next-line
+    [`@media (max-width: ${MOBILE_WIDTH}px)`]: { paddingLeft: 0, width: "100%" }
+  },
+  boxLogo: {
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: BORDER_RADIUS,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  flexTitle: {
+    alignSelf: "flex-start",
     marginBottom: theme.spacing(1)
+  },
+  flexText: {
+    alignSelf: "flex-start"
   },
   fab: {
     marginTop: theme.spacing(1),
+    alignSelf: "flex-end",
     display: "flex",
     flexDirection: "row-reverse"
   },
