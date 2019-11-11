d8cb47f5298acfd374c0f69e44facb910be440ec
barrens <barrens@okbsapr.ru>
2019-11-11 14:18:09 +0300

    Change site address in ref generation

--- backend/EGifts/EGifts/Handlers/BuyGiftRefHandler.cs
+++ backend/EGifts/EGifts/Handlers/BuyGiftRefHandler.cs
@@ -15,7 +15,7 @@ namespace EGifts.Handlers
     class BuyGiftRefHandler : IRequestHandler
     {
         //TODO: вынести в бд!
-        const string BaseUrl = "localhost:3000/view/";
+        const string BaseUrl = "e-gifts.site/view/";
         public BaseMessage Handle(HttpContext context)
         {
             var requestData = context.Request.Query;
