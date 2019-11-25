ed545e1bddbad96252ca4040cd7a6497dd8afeeb
Denis Stasyev <denisstasyev@gmail.com>
2019-11-25 14:16:16 +0300

    Change warning color

--- frontend/src/configs/CSSvariables.js
+++ frontend/src/configs/CSSvariables.js
@@ -5,7 +5,7 @@ export const BORDER_RADIUS = 20;
 
 export const COLOR_BACKGROUND_DEFAULT_LIGHT = "#fff";
 export const COLOR_BACKGROUND_PAPER_LIGHT = "#f3f3f3";
-export const COLOR_BACKGROUND_WARNING_LIGHT = "#ffe7e7";
+export const COLOR_BACKGROUND_WARNING_LIGHT = "#ffd4d4";
 export const COLOR_BACKGROUND_SUCCESS_LIGHT = "#a8ffbc";
 export const COLOR_PRIMARY_MAIN = "#2d53fe";
 export const COLOR_SECONDARY_MAIN = "#00ff39";
